2c245bb9536f1de3793c89cc104633018d4ba576
oranedir <39119119+oranedir@users.noreply.github.com>
2018-05-11 17:37:41 +0200

    ajout style

--- index.php
+++ index.php
@@ -6,19 +6,22 @@
     <link rel="stylesheet" href="style.css"/>
 </head>
 <body>
+
     <form method="post" action="traitement.php">
-        <p>
-            <label for="pseudo" > Votre pseudo: </label>
-            <input type="text" name="pseudo" id="pseudo" required autofocus/>
-            <label for="mdp" > Votre mot de passe : </label>
-            <input type="password" name="mdp" id="mdp" required/>
-            <input type="submit" value="Envoyer"/>
-        </p>
-        <?php
-           if(isset($erreur)) {
-               echo $erreur;
-           }
-        ?>
+        <h1> Se connecter         </h1>
+        <label for="pseudo" > Votre identifiant: </label>
+        <input type="text" name="pseudo" id="pseudo" required autofocus placeholder="prénom(orane)"/>
+        <label for="mdp" > Votre mot de passe : </label>
+        <input type="password" name="mdp" id="mdp" required placeholder="password(stage)"/>
+      <!--   <p id="erreur">
+             <?php /*
+            if(isset($erreur)) {
+                echo $erreur;
+            } */
+            ?>
+        </p> -->
+        <input type="submit" value="CONNEXION"/>
+
     </form>
 </body>
 </html>
--- traitement.php
+++ traitement.php
@@ -5,14 +5,17 @@
  * Date: 09/05/2018
  * Time: 12:05
  */
-$erreur = "nom d'utilisateur ou mot de passe invalide";
+$erreur = " <p style='color:red; font-style: italic;font-size: small; text-align: center;'> nom d'utilisateur et mot de passe invalide </p>";
 $nom = $_POST ["pseudo"];
-    if (($_POST["pseudo"] == "orane")&&($_POST ["mdp"] == "stage")) echo "Bienvenue $nom !";
-    /*header ('location: http://www.commentcamarche.net/');*/
-
-
+    if (($_POST["pseudo"] == "orane")&&($_POST ["mdp"] == "stage")) {
+        require __DIR__ . "/accueil.php";
+        /*header ('location: http://www.commentcamarche.net/');*/
+    }
+    elseif (($_POST["pseudo"]== "orane") && ($_POST ["mdp"] !== "stage" )) {
+        require __DIR__ . "/index.php";
+        echo "<p style='color:red; font-style: italic;font-size: small; text-align: center;'> mot de passe incorrect </p>"; }
     else {
         require __DIR__ . "/index.php";
+        echo $erreur;
     }
-
 ?>
